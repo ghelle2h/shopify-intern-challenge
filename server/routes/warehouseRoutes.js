a1562dbb1c56149326c4fca74e21c1adf3a8c600
Hamza Ghelle <hamzaghellegmail.com>
2022-01-19 12:28:34 -0500

    warehouse file to handle routes dealing with warehouses

--- server/routes/warehouseRoutes.js
+++ server/routes/warehouseRoutes.js
@@ -1,4 +1,24 @@
 const express = require('express');
+const db = require('../db/dbConn');
 const router = express.Router();
+
+router.get("/add_warehouse", (req, res) => {
+  res.render("warehouseForm")
+});
+
+router.post("/add_warehouse", (req, res) => {
+  const {warehouse_name, city, street_address} = req.body;
+  const queryInsert = `
+  INSERT INTO 
+    warehouse(name, city, street_address)
+  VALUES
+    ($1, $2, $3)
+  RETURNING *;
+  `
+  db.query(queryInsert, [warehouse_name, city, street_address])
+    .then(() => {
+      res.redirect("/")
+    });
+});
 
 module.exports = router
