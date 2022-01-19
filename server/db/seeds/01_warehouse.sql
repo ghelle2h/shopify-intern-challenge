e6b597aec46fc452dc34447a1990613b23cbbb7b
Hamza Ghelle <hamzaghellegmail.com>
2022-01-19 12:23:44 -0500

    seed file to insert seed information to database

---
+++ server/db/seeds/01_warehouse.sql
@@ -0,0 +1,3 @@
+INSERT INTO warehouse(name, city, street_address) VALUES('Amazon', 'St. Johns', '123 the amazing blvd');
+INSERT INTO warehouse(name, city, street_address) VALUES('Walmart', 'Oshawa', '123 the wal to mart street');
+INSERT INTO warehouse(name, city, street_address) VALUES('BestBuy', 'Toronto', '123 the best buy road');
