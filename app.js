6a0fd2fd7e7f2c22cee02d91cde240b0050a12a8
Hamza Ghelle <hamzaghellegmail.com>
2022-01-19 12:34:21 -0500

    conntected database and setup routes

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const logger = require('morgan');
 
 const warehouseRoutes = require("./server/routes/warehouseRoutes");
 const itemRoutes = require("./server/routes/itemRoutes");
+const indexRoute = require("./server/routes/index");
 const db = require('./server/db/dbConn')
 
 const app = express();
 db.connect()
 
-db.query(`SELECT * FROM inventory;`).then(data => console.log(data.rows))
+app.set("view engine", "ejs");
 
 
 app.use(logger('dev'));
@@ -17,7 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', itemRoutes);
-app.use('/users', warehouseRoutes);
+app.use('/', indexRoute);
+app.use('/item', itemRoutes);
+app.use('/warehouse', warehouseRoutes);
+
+
 
 module.exports = app;
